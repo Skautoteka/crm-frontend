c910508b6d51e72f8e885dee625c7df35e9bdf67
Damian <dkowalski.1997@gmail.com>
2024-04-23 02:08:31 +0200

    added logout button

--- libs/ui/src/lib/sidenav/components/sidenav-menu/sidenav-menu.component.ts
+++ libs/ui/src/lib/sidenav/components/sidenav-menu/sidenav-menu.component.ts
@@ -7,13 +7,14 @@ import {ClassBinder} from "@skautoteka-frontend/common";
 import {SidenavElement} from "../../interfaces";
 import {NgFor} from "@angular/common";
 import {SidenavMenuElementComponent} from "../sidenav-menu-element/sidenav-menu-element.component";
+import {IconComponent} from "../../../icon";
 
 @Component({
   selector: 'skt-ui-sidenav-menu',
   templateUrl: './sidenav-menu.component.html',
   styleUrl: './sidenav-menu.component.scss',
   standalone: true,
-  imports: [NgFor, SidenavMenuElementComponent],
+  imports: [NgFor, SidenavMenuElementComponent, IconComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   providers: [ClassBinder]
