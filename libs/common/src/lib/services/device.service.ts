2ec7ef247b9a22dece921458b4b54844a27d9c7f
Damian <dkowalski.1997@gmail.com>
2024-04-25 00:50:45 +0200

    work in progress

--- libs/common/src/lib/services/device.service.ts
+++ libs/common/src/lib/services/device.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { map, Observable } from 'rxjs';
 
-@Injectable()
-export class DeviceService {}
+@Injectable({ providedIn: 'root' })
+export class DeviceService {
+  get isMobile$(): Observable<boolean> {
+    return this.breakpointObserver
+      .observe(['(max-width: 850px)'])
+      .pipe(map((state) => state.matches));
+  }
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+}
--- libs/ui/src/lib/sidenav/components/sidenav/sidenav.component.ts
+++ libs/ui/src/lib/sidenav/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import {
   ChangeDetectionStrategy,
+  ChangeDetectorRef,
   Component,
   Input,
   ViewEncapsulation,
@@ -7,13 +8,15 @@ import {
 import { ClassBinder } from '@skautoteka-frontend/common';
 import { SidenavMenuComponent } from '../sidenav-menu/sidenav-menu.component';
 import { SidenavElement } from '../../interfaces';
+import { DeviceService } from '../../../../../../common/src/lib/services/device.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'skt-ui-sidenav',
   styleUrl: './sidenav.component.scss',
   templateUrl: './sidenav.component.html',
   standalone: true,
-  imports: [SidenavMenuComponent],
+  imports: [SidenavMenuComponent, NgIf, AsyncPipe],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   providers: [ClassBinder],
@@ -21,7 +24,22 @@ import { SidenavElement } from '../../interfaces';
 export class SidenavComponent {
   @Input({ required: true }) elements: SidenavElement[] = [];
 
-  constructor(classBinder: ClassBinder) {
+  private isVisible = true;
+
+  constructor(
+    classBinder: ClassBinder,
+    public device: DeviceService,
+    private cdRef: ChangeDetectorRef
+  ) {
     classBinder.bind('skt-ui-sidenav');
+  }
+
+  get isMenuVisible(): boolean {
+    return this.isVisible;
+  }
+
+  public toggleVisible(): void {
+    this.isVisible = !this.isVisible;
+    this.cdRef.detectChanges();
   }
 }
