2896fc9051b8e17d461a9150ab70f029cc215571
Damian <dkowalski.1997@gmail.com>
2024-04-26 01:25:11 +0200

    added header for tasks

--- apps/skautoteka-frontend/src/app/features/tasks/components/tasks-header/tasks-header.component.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/components/tasks-header/tasks-header.component.ts
@@ -4,20 +4,18 @@ import {
   ViewEncapsulation,
 } from '@angular/core';
 import { ClassBinder } from '@skautoteka-frontend/common';
-import { TasksHeaderComponent } from '../tasks-header/tasks-header.component';
 
 @Component({
   standalone: true,
-  selector: 'skt-tasks',
-  styleUrl: './tasks.component.scss',
-  templateUrl: 'tasks.component.html',
+  selector: 'skt-tasks-header',
+  styleUrl: './tasks-header.component.scss',
+  templateUrl: 'tasks-header.component.html',
   providers: [ClassBinder],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TasksHeaderComponent],
 })
-export class TasksComponent {
+export class TasksHeaderComponent {
   constructor(classBinder: ClassBinder) {
-    classBinder.bind('skt-tasks');
+    classBinder.bind('skt-tasks-header');
   }
 }
--- apps/skautoteka-frontend/src/app/features/tasks/index.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/index.ts
@@ -1,1 +1,2 @@
 export * from './components/tasks/tasks.component';
+export * from './components/tasks-header/tasks-header.component';
