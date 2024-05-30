fa2c9defe73478454560d19ec60cfa564c154aa9
Damian Kowalski <dkowalski.1997@gmail.com>
2024-05-31 00:30:06 +0200

    added team comparison in task detail view

--- apps/skautoteka-frontend/src/app/features/tasks/components/tasks/tasks.component.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/components/tasks/tasks.component.ts
@@ -14,6 +14,7 @@ import {
   SideContentSectionComponent
 } from '@skautoteka-frontend/ui';
 import { TasksContentComponent } from '../tasks-content/tasks-content.component';
+import { TasksTeamsComponent } from '../tasks-teams/tasks-teams.component';
 
 @Component({
   standalone: true,
@@ -31,6 +32,7 @@ import { TasksContentComponent } from '../tasks-content/tasks-content.component'
     SideContentComponent,
     SideContentSectionComponent,
     ButtonComponent,
+    TasksTeamsComponent
   ],
 })
 export class TasksComponent {
--- apps/skautoteka-frontend/src/app/features/tasks/index.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/index.ts
@@ -1,1 +1,2 @@
 export * from './components/tasks/tasks.component';
+export * from './components/tasks-teams/tasks-teams.component';
