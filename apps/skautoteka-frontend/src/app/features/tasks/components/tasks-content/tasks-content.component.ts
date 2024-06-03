9fc5ee7a45d1483bb1999378dad4ff65153a2835
Damian <dkowalski.1997@gmail.com>
2024-06-03 22:04:20 +0200

    changes in mobile layout

--- apps/skautoteka-frontend/src/app/features/tasks/components/tasks-content/tasks-content.component.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/components/tasks-content/tasks-content.component.ts
@@ -11,7 +11,8 @@ import {
   TabsComponent,
 } from '@skautoteka-frontend/ui';
 import { TasksService } from '../../services';
-
+import { AsyncPipe } from '@angular/common';
+import { DeviceService } from '../../../../../../../../libs/common/src/lib/services/device.service';
 
 @Component({
   standalone: true,
@@ -21,10 +22,20 @@ import { TasksService } from '../../services';
   providers: [ClassBinder],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [TabsComponent, TabComponent, ListCardComponent, LabelComponent],
+  imports: [
+    TabsComponent,
+    TabComponent,
+    ListCardComponent,
+    LabelComponent,
+    AsyncPipe,
+  ],
 })
 export class TasksContentComponent {
-  constructor(classBinder: ClassBinder, public tasksService: TasksService) {
+  constructor(
+    classBinder: ClassBinder,
+    public tasksService: TasksService,
+    public device: DeviceService
+  ) {
     classBinder.bind('skt-tasks-content');
   }
 
