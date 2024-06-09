091d3940a25e90d94948b10c39d74ba7576a4fe5
Damian <dkowalski.1997@gmail.com>
2024-06-09 16:36:52 +0200

    added overlay structure

---
+++ libs/ui/src/lib/overlay/components/modal/modal.component.ts
@@ -0,0 +1,21 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ViewEncapsulation,
+} from '@angular/core';
+import { ClassBinder } from '@skautoteka-frontend/common';
+
+@Component({
+  standalone: true,
+  selector: 'skt-ui-modal',
+  styleUrl: './modal.component.scss',
+  templateUrl: 'modal.component.html',
+  providers: [ClassBinder],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ModalComponent {
+  constructor(classBinder: ClassBinder) {
+    classBinder.bind('skt-ui-modal');
+  }
+}
