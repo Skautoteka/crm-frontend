601335c74f9acea009b8f2ba057a6f47454602e4
Damian <dkowalski.1997@gmail.com>
2024-06-09 02:14:55 +0200

    added tasks side empty component

--- apps/skautoteka-frontend/src/app/features/dashboard/tasks.routes.ts
+++ apps/skautoteka-frontend/src/app/features/dashboard/tasks.routes.ts
@@ -1,20 +1,23 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from '../tasks';
 import { TasksSideContentComponent } from '../tasks/components/tasks-side-content/tasks-side-content.component';
+import { TasksSideEmptyComponent } from '../tasks';
 
 export const TASKS_ROUTES: Routes = [
   {
     path: 'details',
-    component: TasksSideContentComponent,
     children: [
       {
         path: ':id',
-        component: TasksComponent,
+        component: TasksSideContentComponent,
+      },
+      {
+        path: '**',
+        redirectTo: '..',
       },
     ],
   },
   {
     path: '**',
-    redirectTo: 'details',
+    component: TasksSideEmptyComponent,
   },
 ];
