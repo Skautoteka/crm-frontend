b57f6b3eaecd3878371c2545db679aaf9226577f
Jakub-Prus <jakub.prus03@gmail.com>
2024-06-09 23:13:05 +0200

    feat: basic table component

---
+++ libs/ui/src/lib/table/components/table/table.component.ts
@@ -0,0 +1,44 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  Host,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { ClassBinder } from '@skautoteka-frontend/common';
+import { TableService } from '../../services/table.service';
+import { AsyncPipe } from '@angular/common';
+
+export interface TableColumn {
+  key: string;
+  label: string;
+  type: 'string' | 'number' | 'date' | 'boolean' | 'img';
+}
+
+@Component({
+  standalone: true,
+  selector: 'skt-ui-table',
+  styleUrl: './table.component.scss',
+  templateUrl: 'table.component.html',
+  providers: [ClassBinder],
+  imports: [AsyncPipe, CommonModule, DatePipe],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TableComponent {
+  @Input() data: any[] = [];
+  @Input() columns: TableColumn[] = [];
+  constructor(
+    // @Host() private tableService: TableService,
+    private elementRef: ElementRef,
+    classBinder: ClassBinder
+  ) {
+    classBinder.bind('skt-ui-table');
+  }
+
+  get nativeElement(): HTMLElement {
+    return this.elementRef.nativeElement;
+  }
+}
