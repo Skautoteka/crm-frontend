222bc38186aeefc018a9d0233d65415caf33e2b9
Jakub-Prus <jakub.prus03@gmail.com>
2024-06-10 19:00:23 +0200

    polish dates

--- apps/skautoteka-frontend/src/app/app.config.ts
+++ apps/skautoteka-frontend/src/app/app.config.ts
@@ -1,6 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import myLocalePl from '@angular/common/locales/pl';
+registerLocaleData(myLocalePl);
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(appRoutes)],
