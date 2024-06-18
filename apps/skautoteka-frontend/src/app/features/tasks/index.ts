7f6af45dd787ec2bb1fdc81f60b7e9d9d22c8e7a
Damian <dkowalski.1997@gmail.com>
2024-06-18 22:57:31 +0200

    removed circular dependency

--- apps/skautoteka-frontend/src/app/features/tasks/index.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/index.ts
@@ -3,3 +3,4 @@ export * from './components/tasks-teams/tasks-teams.component';
 export * from './components/tasks-basic-info/tasks-basic-info.component';
 export * from './components/tasks-reports/tasks-reports.component';
 export * from './components/tasks-side-empty/tasks-side-empty.component';
+export * from './services';
