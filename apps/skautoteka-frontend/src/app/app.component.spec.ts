737c6c872467e4054d7d9d2d3f84fdc53a8055d8
Damian <dkowalski.1997@gmail.com>
2024-06-18 23:47:14 +0200

    test fix

--- apps/skautoteka-frontend/src/app/app.component.spec.ts
+++ apps/skautoteka-frontend/src/app/app.component.spec.ts
@@ -1,25 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent, NxWelcomeComponent, RouterTestingModule]
+      imports: [AppComponent]
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('creates successfully', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome skautoteka-frontend');
-  });
-
-  it(`should have as title 'skautoteka-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('skautoteka-frontend');
+    expect(fixture).toBeTruthy();
   });
 });
