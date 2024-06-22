05c3a14ecdfe01f296515371ef7e49e2b6e952e6
Damian Kowalski <dkowalski.1997@gmail.com>
2024-06-22 15:58:16 +0200

    column redefinition

--- apps/skautoteka-frontend/src/app/features/teams/components/teams-content/teams-content.component.ts
+++ apps/skautoteka-frontend/src/app/features/teams/components/teams-content/teams-content.component.ts
@@ -15,10 +15,9 @@ import { TeamsService } from '../../services/teams.service';
 })
 export class TeamsContentComponent {
   public tableDef = [
-    { name: 'Zdjecie', width: '10%', hidden: true },
-    { name: 'Nazwa', width: '10%' },
-    { name: 'Liga', width: '20%' },
-    { name: 'Kraj', width: '30%' },
+    { name: 'Zdjecie', width: '15%', hidden: true },
+    { name: 'Nazwa', width: '35%' },
+    { name: 'Kraj', width: '20%' },
     { name: 'Miasto', width: '30%' }
   ]
 
