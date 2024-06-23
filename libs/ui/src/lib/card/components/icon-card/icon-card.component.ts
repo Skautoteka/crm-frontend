52445eb62541aaef9f942200cbf0e08a121cbdba
Damian <dkowalski.1997@gmail.com>
2024-06-23 15:40:34 +0200

    added icon card component template

--- libs/ui/src/lib/card/components/icon-card/icon-card.component.ts
+++ libs/ui/src/lib/card/components/icon-card/icon-card.component.ts
@@ -1,16 +1,20 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, ViewEncapsulation } from '@angular/core';
 import { ClassBinder } from '@skautoteka-frontend/common';
+import { IconComponent } from '../../../icon';
 
 @Component({
   standalone: true,
-  selector: 'skt-ui-list-card',
-  styleUrl: './list-card.component.scss',
-  templateUrl: './list-card.component.html',
+  selector: 'skt-ui-icon-card',
+  styleUrl: './icon-card.component.scss',
+  templateUrl: './icon-card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
+  imports: [IconComponent],
   providers: [ClassBinder]
 })
 export class IconCardComponent {
+  public iconName = input<string>('');
+
   constructor(classBinder: ClassBinder) {
     classBinder.bind('skt-ui-icon-card');
   }
--- libs/ui/src/lib/card/index.ts
+++ libs/ui/src/lib/card/index.ts
@@ -1,1 +1,2 @@
 export * from './components/list-card/list-card.component';
+export * from './components/icon-card/icon-card.component';
