e469ef36b97c431774902f869950db2a5f57d529
Damian <dkowalski.1997@gmail.com>
2024-06-23 15:56:54 +0200

    updated icon card component to change icon colors

--- libs/ui/src/lib/card/components/icon-card/icon-card.component.ts
+++ libs/ui/src/lib/card/components/icon-card/icon-card.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, input, OnInit, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, input, OnInit, signal, ViewEncapsulation } from '@angular/core';
 import { ClassBinder } from '@skautoteka-frontend/common';
 import { IconComponent } from '../../../icon';
 import { IconCardType } from '../../interfaces';
@@ -17,11 +17,33 @@ export class IconCardComponent implements OnInit {
   public iconName = input<string>('');
   public type = input<IconCardType>('simple');
 
+  public iconColor = signal<string>('');
+
   constructor(private _classBinder: ClassBinder) {
     _classBinder.bind('skt-ui-icon-card');
+    this._updateIconColor();
   }
 
   public ngOnInit(): void {
     this._classBinder.bind('skt-ui-icon-card--' + this.type());
+  }
+
+  private _updateIconColor(): void {
+    effect(
+      () => {
+        const type = this.type();
+        switch (type) {
+          case 'simple':
+            this.iconColor.set('var(--blue-dark)');
+            break;
+          case 'success':
+            this.iconColor.set('var(--primary-5)');
+            break;
+          case 'warn':
+            this.iconColor.set('var(--warn)');
+        }
+      },
+      { allowSignalWrites: true }
+    );
   }
 }
