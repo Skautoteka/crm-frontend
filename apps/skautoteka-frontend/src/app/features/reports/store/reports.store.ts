d74ac09e12d530d0da8497eb63538409cf161f00
Damian Kowalski <dkowalski.1997@gmail.com>
2024-10-01 00:07:37 +0200

    added report store

---
+++ apps/skautoteka-frontend/src/app/features/reports/store/reports.store.ts
@@ -0,0 +1,24 @@
+import { InputConfig } from "@skautoteka-frontend/ui";
+import { Report } from "../interfaces/report";
+import { signalStore, withState } from "@ngrx/signals";
+import { withReportsMethods } from "./reports.methods";
+
+export type ReportsStoreState = {
+  reports: Report[];
+  isLoading: boolean;
+  activeReport: Report | null;
+  createFields: InputConfig | null;
+}
+
+const initialState: ReportsStoreState = {
+  reports: [],
+  isLoading: false,
+  activeReport: null,
+  createFields: null
+}
+
+export const ReportsStore = signalStore(
+  { providedIn: 'root' },
+  withState(initialState),
+  withReportsMethods()
+)
