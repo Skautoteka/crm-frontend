871a340c6e5eff3c3bb6eea9cdea10c85e79c9db
Jakub-Prus <jakub.prus03@gmail.com>
2024-10-09 22:35:06 +0200

    user profile outline component

---
+++ apps/skautoteka-frontend/src/app/features/dashboard/components/dashboard-user-profile/dashboard-user-profile.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ClassBinder } from '@skautoteka-frontend/common';
+import {
+  IconCardComponent,
+  LabelComponent,
+  LabelContainerComponent,
+  ButtonComponent,
+  InputComponent,
+  InputContainerComponent,
+  InputViewService
+} from '@skautoteka-frontend/ui';
+import { AsyncPipe } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'skt-dashboard-user-profile',
+  styleUrl: './dashboard-user-profile.component.scss',
+  templateUrl: 'dashboard-user-profile.component.html',
+  providers: [ClassBinder, InputViewService],
+  imports: [
+    IconCardComponent,
+    LabelComponent,
+    LabelContainerComponent,
+    InputComponent,
+    ButtonComponent,
+    InputContainerComponent,
+    AsyncPipe
+  ],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class DashboardUserProfileComponent {
+  constructor(classBinder: ClassBinder) {
+    classBinder.bind('skt-dashboard-user-profile');
+  }
+}
