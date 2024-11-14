12620cb86ae019965c6129e6496d4852df1f63dc
Jakub-Prus <jakub.prus03@gmail.com>
2024-11-14 18:36:58 +0100

    use of assigned reports

--- apps/skautoteka-frontend/src/app/features/tasks/components/tasks-reports/tasks-reports.component.ts
+++ apps/skautoteka-frontend/src/app/features/tasks/components/tasks-reports/tasks-reports.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ClassBinder } from '@skautoteka-frontend/common';
 import { LabelComponent, LabelContainerComponent, ListCardComponent } from '@skautoteka-frontend/ui';
+import { TasksStore } from '../../store/tasks.store';
 
 @Component({
   standalone: true,
@@ -8,11 +10,15 @@ import { LabelComponent, LabelContainerComponent, ListCardComponent } from '@ska
   styleUrl: './tasks-reports.component.scss',
   templateUrl: 'tasks-reports.component.html',
   providers: [ClassBinder],
-  imports: [LabelComponent, LabelContainerComponent, ListCardComponent],
+  imports: [LabelComponent, LabelContainerComponent, ListCardComponent, CommonModule],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TasksReportsComponent {
+  public tasksStore = inject(TasksStore);
+
+  public assignedReports = this.tasksStore.assignedReports;
+
   constructor(classBinder: ClassBinder) {
     classBinder.bind('skt-tasks-reports');
   }
