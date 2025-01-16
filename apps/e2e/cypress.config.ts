63f2c7d7b3fbf8719b546682c6c09d02e9d5880c
Jakub-Prus <jakub.prus03@gmail.com>
2025-01-16 23:00:54 +0100

    tmp config for cypress and github actions

--- apps/e2e/cypress.config.ts
+++ apps/e2e/cypress.config.ts
@@ -1,17 +1,17 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
-
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, {
-      cypressDir: 'src',
+      cypressDir: 'apps/e2e/src',
       webServerCommands: {
         default: 'nx run skautoteka-frontend:serve:development',
         production: 'nx run skautoteka-frontend:serve:production'
       },
       ciWebServerCommand: 'nx run skautoteka-frontend:serve-static'
     }),
+    supportFile: 'apps/e2e/src/support/e2e.ts',
     baseUrl: 'http://localhost:4200'
   }
 });
